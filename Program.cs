6e39305b16b9a8de2124808262cfc266bf661ba8
Hyaku Ju Yon <ichitora114@outlook.com>
2021-07-22 05:08:33 +0300

    Changes_1

--- Program.cs
+++ Program.cs
@@ -2,24 +2,22 @@
 
 namespace Mod_4._3_Pract
 {
-    class Program
-    {
+	class Program
+	{
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Write your name");
 
 			var name = Console.ReadLine();
 
-			Console.WriteLine("Your name spelled as: ");
+			for (int i = name.Length - 1; i >= 0; i--)
 
-			foreach (var ch in name)
 			{
-				Console.Write(ch + " ");
-			}
-			
-			Console.WriteLine("The first letter of your name is {0}:", name[0]);
 
+				Console.Write(name[i] + "");
+
+			}
 			Console.ReadKey();
 		}
-    }
+	}
 }
