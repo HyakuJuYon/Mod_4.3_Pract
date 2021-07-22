9b61da79051743b2435d896be33bb013c7875a17
Hyaku Ju Yon <ichitora114@outlook.com>
2021-07-22 04:30:48 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mod_4._3_Pract
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Write your name");
+
+			var name = Console.ReadLine();
+
+			Console.WriteLine("Your name spelled as: ");
+
+			foreach (var ch in name)
+			{
+				Console.Write(ch + " ");
+			}
+			
+			Console.WriteLine("The first letter of your name is {0}:", name[0]);
+
+			Console.ReadKey();
+		}
+    }
+}
